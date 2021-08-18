2a1eadb93ae86f34d92cdbb835be07c0e3fe25a3
Heather Dalton <79050392+hjdalton@users.noreply.github.com>
2021-08-18 12:33:26 +0100

    rspec tests and space class fixed

--- lib/space.rb
+++ lib/space.rb
@@ -2,26 +2,30 @@ require 'pg'
 
 class Space
 
-  attr_reader :id, :space_name, :description, :price, :user_id
+  attr_reader :id, :space_name, :description, :price, :current_user
 
-  def initialize(id:, space_name:, description:, price:, user_id:, email:)
+  def initialize(id:, space_name:, description:, price:, current_user:)
     @id = id
     @space_name = space_name
     @description = description
     @price = price
+    @current_user = current_user
   end
 
 
 
-  def self.create(space_name:, description:, price:, email:)
+  def self.create(space_name:, description:, price:, current_user:)
     if ENV['ENVIRONMENT'] == 'test'
       connection = PG.connect(dbname: 'makersbnb_test')
     else
       connection = PG.connect(dbname: 'makersbnb')
     end
-    result = connection.exec("INSERT INTO spaces (space_name, description, price, user_id) VALUES($1, $2, $3, (SELECT id FROM users WHERE email = '#{email}')) RETURNING id, space_name, description, price, user_id;", [space_name, description, price, user_id])
-    Space.new(id: result[0]['id'], space_name: result[0]['space_name'], description: result[0]['description'], price: result[0]['price'], user_id: result[0]['user_id'])
+    result = connection.exec("INSERT INTO spaces (space_name, description, price, user_id) VALUES($1, $2, $3, (SELECT id FROM users WHERE email = $4)) RETURNING id, space_name, description, price, user_id;", [space_name, description, price, current_user])
+    Space.new(id: result[0]['id'], space_name: result[0]['space_name'], description: result[0]['description'], price: result[0]['price'], current_user: result[0]['user_id'])
   end
+
+  #(' || $1 || ' ' || $2 ||  ' ' || $3 || ' ' || $4 ||'')’))
+  #'#{space_name}', '#{description}', '#{price}'
 
 
   def self.all
@@ -36,7 +40,8 @@ class Space
       id: space['id'],
       space_name: space['space_name'],
       description: space['description'],
-      price: space['price']
+      price: space['price'],
+      current_user: space['user_id']
       )
     end
   end
