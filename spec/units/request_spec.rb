9e5a2f94d10223c33446d7c47fabb4d0d93abbad
Heather Dalton <79050392+hjdalton@users.noreply.github.com>
2021-08-19 14:26:12 +0100

    amended truncate for test database

--- spec/units/request_spec.rb
+++ spec/units/request_spec.rb
@@ -1,18 +1,6 @@
 require 'request'
-require 'booking'
 
 describe Request do
-
-  describe '.received' do
-    it 'returns any requests that have been made for a homeowners space' do
-    
-      received = Request.received(user_id: 1)
-      
-      expect(received).to be_a Request
-      expect(receive.space_id).to eq '1'
-    end
-  end
-
   describe '.made' do
     it 'returns any booking requests that the user has made' do
 
@@ -24,4 +12,13 @@ describe Request do
     end
   end
 
+  describe '.received' do
+    it 'returns any requests that have been made for a homeowners space' do
+    
+      received = Request.received(user_id: 1)
+      
+      expect(received).to be_a Request
+      expect(receive.space_id).to eq '1'
+    end
+  end
 end
