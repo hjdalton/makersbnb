ac92c5939f7a3058341a278f1584c66ce273f118
Ben Davis <ben.david.davis16@gmail.com>
2021-08-19 15:50:09 +0100

    accept and reject methods and test

--- lib/booking.rb
+++ lib/booking.rb
@@ -1,3 +1,5 @@
+require 'pg'
+
 class Booking
 
   attr_reader :id, :space_id, :user_id, :start_date, :end_date, :status
@@ -26,6 +28,40 @@ class Booking
       $3, $4, 'pending') 
       RETURNING id, space_id, user_id, start_date, end_date, status;", 
       [space_id, user_id, start_date, end_date])
+    
+    Booking.new(id: result[0]['id'],space_id: result[0]['space_id'], user_id: result[0]['user_id'], 
+      start_date: result[0]['start_date'], end_date: result[0]['end_date'], status: result[0]['status'])
+  end
+
+  def self.accept(id:)
+    if ENV['ENVIRONMENT'] == 'test'
+      connection = PG.connect(dbname: 'makersbnb_test')
+    else
+      connection = PG.connect(dbname: 'makersbnb')
+    end
+    result = connection.exec(
+      "UPDATE bookings
+      SET status = 'accepted'
+      WHERE id = #{id} 
+      RETURNING id, space_id, user_id, start_date, end_date, status;" 
+      )
+    
+    Booking.new(id: result[0]['id'],space_id: result[0]['space_id'], user_id: result[0]['user_id'], 
+      start_date: result[0]['start_date'], end_date: result[0]['end_date'], status: result[0]['status'])
+  end
+
+  def self.reject(id:)
+    if ENV['ENVIRONMENT'] == 'test'
+      connection = PG.connect(dbname: 'makersbnb_test')
+    else
+      connection = PG.connect(dbname: 'makersbnb')
+    end
+    result = connection.exec(
+      "UPDATE bookings
+      SET status = 'rejected'
+      WHERE id = #{id} 
+      RETURNING id, space_id, user_id, start_date, end_date, status;" 
+      )
     
     Booking.new(id: result[0]['id'],space_id: result[0]['space_id'], user_id: result[0]['user_id'], 
       start_date: result[0]['start_date'], end_date: result[0]['end_date'], status: result[0]['status'])
--- spec/units/booking_spec.rb
+++ spec/units/booking_spec.rb
@@ -1,3 +1,4 @@
+require 'pg'
 require 'booking'  
 
 describe '.create' do
@@ -8,5 +9,19 @@ describe '.create' do
     expect(booking.start_date).to eq '2020-08-19'
     expect(booking.end_date).to eq '2020-08-20'
     expect(booking.status).to eq 'pending'
+  end
+end
+
+describe '.accept' do
+  it 'accepts another users booking request' do
+    accept = Booking.accept(id: '1')
+    expect(accept.status).to eq 'accepted'
+  end
+end
+
+describe '.reject' do
+  it 'reject another users booking request' do
+    reject = Booking.reject(id: '1')
+    expect(reject.status).to eq 'rejected'
   end
 end
